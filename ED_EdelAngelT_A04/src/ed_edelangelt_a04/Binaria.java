4a8d4eaffebb161cf96c4d5a5e09efc40a83dbac
LuisGuillermo <LuisGuillermo@MEMO>
2017-08-17 21:12:50 -0500

    Actividad4

---
+++ ED_EdelAngelT_A04/src/ed_edelangelt_a04/Binaria.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ed_edelangelt_a04;
+
+/**
+ *
+ * @author LuisGuillermo
+ */
+public class Binaria extends Fetch implements Ordenamientos{
+    public int Fecth(int numero){
+        int n = v.length;
+        int inicio = 0;
+        int centro;
+        int fin = n-1;
+        
+    while(inicio <= fin){
+        centro = (inicio + fin)/2;
+        
+        if(v[centro] == numero)
+          return numero;
+        
+        if(numero < v[numero]){
+          fin = centro-1;
+    }else{
+        inicio = centro+1;   
+            }    
+    } 
+    return -1;
+}
+
+    @Override
+    public void seleccion() {
+        int menor, aux;
+        for(int i=0;i<v.length;i++){
+            menor=i;
+            for(int j=i+1;j<v.length;j++){
+                if(v[j]<v[menor]){
+                    menor=j;
+                }
+            }
+            if(i != menor){
+            aux=v[i];
+            v[i]=v[menor];
+            v[menor]=aux;
+        }
+    }
+}
+}
---
+++ ED_EdelAngelT_A04/src/ed_edelangelt_a04/Secuencial.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ed_edelangelt_a04;
+
+import ed_edelangelt_a04.Fetch;
+
+/**
+ *
+ * @author LuisGuillermo
+ */
+public class Secuencial extends Fetch{
+    int Fecth(int n){
+            for(int i=0;i<v.length;i++){
+                if(v[i] == n){
+                    return n;
+                }
+            }
+            return -1;
+        }
+    }
